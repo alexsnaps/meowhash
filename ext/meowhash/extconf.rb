9256b1c08eb91676d20ac798f6ddbe7c5ec0bc62
Tjad Clark <tjataz@gmail.com>
2019-03-24 17:10:49 +0200

    Use environment CC instead of Ruby build CC

--- ext/meowhash/extconf.rb
+++ ext/meowhash/extconf.rb
@@ -2,6 +2,6 @@ require "mkmf"
 
 $CFLAGS = "-O3 -Wall -mavx -maes"
 
-abort "Requires CLANG!" unless CONFIG['CC']&.end_with? 'clang'
+abort "Requires CLANG!" unless ENV['CC']&.end_with? 'clang'
 
 create_makefile "meowhash/meowhash"
